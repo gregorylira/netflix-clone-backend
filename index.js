c0fc4eb231469f9e652ee392290ab88cfa64508b
gregorylira <gregory.lira007@gmail.com>
2022-04-26 18:37:50 -0300

    🌟 feat: base project + sistem register

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected to mongoDB"))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
+
+app.listen(8800, () => {
+  console.log("backend server is running!");
+});
